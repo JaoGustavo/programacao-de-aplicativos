1bd4924f33c85b8d1a2eae1b2dea65fc253e94fe
joao gustavo <j.gustavo.s.v.197@gmail.com>
2026-02-24 11:49:58 -0300

    exercicios

---
+++ exercicio02.py
@@ -0,0 +1,6 @@
+ano_nascimento = int(input("digite sua data de nascimento"))
+ano_atual = int(input("digite o ano atual"))
+
+subtracao = ano_atual - ano_nascimento
+
+print("idade que fará esse ano", subtracao)
