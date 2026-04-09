f45dd2b2924188ec4cdf41a14cb24e1736f099bb
joao gustavo <j.gustavo.s.v.197@gmail.com>
2026-04-09 09:58:07 -0300

    exercicios dia 09/04

---
+++ exercicios-avaliativos(30)/inversor_de_ordem_28.py
@@ -0,0 +1,13 @@
+numeros = []
+
+numeros.append_int(input("digite o primeiro nunero: "))
+numeros.append_int(input("digite o primeiro nunero: "))
+numeros.append_int(input("digite o primeiro nunero: "))
+numeros.append_int(input("digite o primeiro nunero: "))
+
+print("lista na ordem ineversa")
+
+print(numeros.pop())
+print(numeros.pop())
+print(numeros.pop())
+print(numeros.pop())
--- exercicios-avaliativos(30)/o_maior_numero_18.py
+++ exercicios-avaliativos(30)/o_maior_numero_18.py
@@ -3,7 +3,7 @@ numeros = [12, 45, 7, 89, 23, 56]
 maior_valor = numeros[0]
 
 for n in numeros:
-    if n > maior_valor:
+    if n > maior_valor
         maior_valor = n
 
-print(f"O maior valor é: {maior_valor}")
+print (f"O maior vslor é: {maior_valor}")
