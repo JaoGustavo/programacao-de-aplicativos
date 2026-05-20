a46b2ce12dd7637d5f568985aeebf6a545320f33
joao gustavo <j.gustavo.s.v.197@gmail.com>
2026-05-20 12:40:27 -0300

    exercicios dia 20/05

--- a_funcao_de_procedimento.py
+++
@@ -1,4 +0,0 @@
-def mostrar_menu():
-    print("menu")
-    print("1 - iniciar")
-    print("2 - sair")
---
+++ escola_digital.py
@@ -0,0 +1,24 @@
+import json
+
+arquivo_nome = "alunos.json"
+
+while True:
+    print("\n1.Cadastrar | 2.Listar | 3.Editar | 4.Excluir | 5.Sair")
+    opçao = input("Opçao: ")
+
+    if opçao == "1":
+        cpf = input("CPF: ")
+        if (aluno["cpf"] == cpf for aluno in lista):
+            print("Erro: CPF já existe!")
+        else:
+            cadastro = {
+                "cpf": cpf,
+                "nome": input("Nome: "),
+                "telefone": input("Telefone: "),
+                "turma": input("Turma: "),
+                "idade": input("Idade: "),
+            }
+            lista.append(cadastro)
+            json.dump(lista, open(arquivo_nome, "w"))
+            print("Salvo!")
+
