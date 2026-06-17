5ecd384d507ff8033e39f2aa25f6cbacc436bde5
joao gustavo <j.gustavo.s.v.197@gmail.com>
2026-06-17 08:17:41 -0300

    exercicios dia 16/06

--- alistamento.py
+++
@@ -1,6 +0,0 @@
-alistamento = ["Ana", "Bruno", "Caio"]
-alistamento.append ("Daniel")
-alistamento.insert(0, "Zuleica")
-
-print("alistamento", alistamento)
-print("quantidades de nomes", len(alistamento))
--- analisador_de_desempenho_em_equipe.py
+++
@@ -1,9 +0,0 @@
-def analisar_vendas(nome, lista_vendas, meta_mensal):
-    media = sum(lista_vendas) / len(lista_vendas)
-    
-    bateu_meta = "bateu" if media >= meta_mensal else "não bateu"
-    
-    return f"O vendedor {nome} teve média de {media:.2f} e {bateu_meta} a meta"
-
-resultado = analisar_vendas("Carlos", [1200, 1500, 1100, 1900], 1400)
-print(resultado)
