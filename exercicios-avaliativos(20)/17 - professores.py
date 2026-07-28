d5d1e5749f70f36041df7b820d76b5df33bec174
joao gustavo <j.gustavo.s.v.197@gmail.com>
2026-07-28 09:51:33 -0300

    exercicios dia 28/07

---
+++ exercicios-avaliativos(20)/17 - professores.py
@@ -0,0 +1,15 @@
+import sqlite3 
+ 
+def inserir_professor(nome, materia, cpf): 
+    try: 
+        conexao = sqlite3.connect('sistema_escola.db') 
+        cursor = conexao.cursor() 
+        cursor.execute("INSERT INTO professores (nome, materia, cpf) VALUES (?,?,?)", (nome, materia, cpf)) 
+        conexao.commit() 
+    except sqlite3.Error: 
+        print("Erro: Este CPF já está cadastrado no sistema!") 
+    finally: 
+        conexao.close() 
+
+# Na linha 7 cursor.execute("INSERTO INTO professores " O insert está escrito errado.
+# O except não captura o erro de sintaxe por que ele é especifico do codigo inteiro, teria que criar um novo except apenas para erro de sintaxe
